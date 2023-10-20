820bdc6dd18a70ca4389d8d6aece8228dcc4196a
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-10-20 11:11:58 +0600

    banner updated

--- src/Components/BrandBanner/BrandBanner.jsx
+++ src/Components/BrandBanner/BrandBanner.jsx
@@ -13,12 +13,13 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Pagination, Navigation } from 'swiper/modules';
+import LeftNavbar from '../leftNavbar/LeftNavbar';
 
 
 
 const BrandBanner = ({images}) => {
     return (
-        <div className='w-[360px] lg:w-2/3'>
+        <div className='w-[360px] lg:w-2/3 flex  '>
         <Swiper
   pagination={{
     type: 'fraction',
@@ -30,16 +31,15 @@ const BrandBanner = ({images}) => {
 {
      images.map(singleImg=><>
           <SwiperSlide> 
-          <div className=' w-96  md:w-full '>
+          <div className=' w-96  md:w-full  '>
                  
-          <img className='relative  w-[355px]  h-[400px]  ' src={singleImg} alt="" />
+          <img className='relative w-36  md:w-[355px]  md:h-[400px]  ' src={singleImg} alt="" />
               
               <div  data-aos-duration="2000"  data-aos="zoom-in-right" className='absolute p-8
-              top-3 lg:top-24 w-96 lg:w-full left-80 lg:space-y-4'> 
-           <p className='font-semibold '>    Sale Offer <span className='text-red-700'>-10% Off</span> This Week</p>
-             <h2 className=' text-xl lg:text-3xl'>Top Trending
-              Headphone 2019</h2>
-          <p className='text-xl'>Starting at <span className='text-red-700 text-3xl'>$589.00</span></p>
+              top-3 lg:top-24 w-96 lg:w-full left-[104px] md:left-80 lg:space-y-4'> 
+           <p className='font-semibold text-sm md:text-xl '>    Sale Offer <span className='text-red-700'>-10% Off</span> This Week</p>
+             <h2 className=' text-xl lg:text-3xl'>and 20% Discount</h2>
+          <p className='text-xl'>Starting at <span className='text-red-700 text-md md:text-3xl'>$89.00</span></p>
               </div>
           </div>
             
@@ -52,6 +52,11 @@ const BrandBanner = ({images}) => {
 
 
 </Swiper>
+
+ 
+  <div className='relative left-60 w-full hidden md:block'>
+  <LeftNavbar></LeftNavbar>
+  </div>
   </div>
     );
 };
